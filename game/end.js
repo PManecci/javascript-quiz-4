bb58cd145f1105e587351c0e5cb645b98508a9d7
manecci <pmanecci@gmail.com>
2021-11-08 21:34:03 -0500

    Final Link Update

--- game/end.js
+++ game/end.js
@@ -30,5 +30,5 @@ saveHighScore = e => {
     highScores.splice(5)
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('highscores/highscores.html')
+    window.location.assign('../index.html')
 }
